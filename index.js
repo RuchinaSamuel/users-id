91e09a6ed340143257f07d174d8cc581f3570994
RuchinaSamuel <ruchinasamuel1998@gamil.com>
2022-03-15 11:16:04 +0100

    users id

---
+++ index.js
@@ -0,0 +1,255 @@
+
+const users1 =  {id: 1,
+ Name: "Leanne Graham",
+ userName: "Bret", 
+ Email: "Sincere@april.biz",
+  Address: {
+ Street: "Kulas Light",
+ Suite: "Apt. 556",
+City: "Gwenborough",
+Zipcode: "92998-3874",
+geo: {
+ lat: "-37.3159",
+ lng: "81.1496",
+ },
+ },
+ phone: "1-770-736-8031 x56442",
+ website: "hildegard.org",
+  Company: {
+ Name: "Romaguera-Crona",
+ catchPhrase: "Multi-layered client-server neural-net",
+  Bs: "harness real-time e-markets",
+  
+ },
+  };
+
+document.getElementById("tableA").innerHTML=  users1.id+"<br>" + "Name:" + users1.Name +"<br>"+ "User Name:" + users1.userName + "<br>"+ "Email:" + users1.Email + "<br>"+ "Street:" + users1.Address.Street + "<br>"+"Suite:" + users1.Address.Suite + "<br>"+ "City:"+ users1.Address.City + "<br>"+ "Zipcode:" + users1.Address.Zipcode + "<br>" + "Lat:" + users1.Address.geo.lat + "<br>" + "Lng:" + users1.Address.geo.lng + "<br>" + "Phone:" + users1.phone + "<br>"+ "Company Name:" + users1.Company.Name + "<br>" +"Catch Phrase:" + users1.Company.catchPhrase + "<br>" + "bs:" + users1.Company.Bs
+
+
+
+const users2 = {id: 2,
+name: "Ervin Howell",
+ username: "Antonette",
+ email: "Shanna@melissa.tv",
+ address: {
+  street: "Victor Plains",
+ suite: "Suite 879",
+ city: "Wisokyburgh",
+  zipcode: "90566-7771",
+ geo: {
+ lat: "-43.9509",
+  lng: "-34.4618",
+    },
+  },
+ phone: "010-692-6593 x09125",
+ website: "anastasia.net",
+ company: {
+ name: "Deckow-Crist",
+ catchPhrase: "Proactive didactic contingency",
+  bs: "synergize scalable supply-chains",
+  },
+    };
+    
+    document.getElementById("tableB").innerHTML= users2.id + "<br>"+"Name:" + users2.name + "<br>" +"Username:" + users2.username + "<br>" + "Email:" + users2.email + "<br>"+ "Street:" + users2.address.street + "<br>" + "Suite:" + users2.address.suite + "<br>"+ "City:"+ users2.address.city + "<br>"+ "Zipcode:" + users2.address.zipcode + "<br>" + "Lat:" + users2.address.geo.lat + "<br>" + "lng:" + users2.address.geo.lng + "<br>" + "phone:" + users2.phone + "<br>" + "Company Name::" + users2.company.name + "<br>" + "Catchphrase:" + users2.company.catchPhrase + "<br>" +"bs:" + users2.company.bs
+
+
+ const users3= {id: 3,
+ name: "Clementine Bauch",
+ username: "Samantha",
+ email: "Nathan@yesenia.net",
+ address: {
+ street: "Douglas Extension",
+  suite: "Suite 847",
+ city: "McKenziehaven",
+ zipcode: "59590-4157",
+ geo: {
+  lat: "-68.6102",
+   lng: "-47.0653",
+ },
+  },
+ phone: "1-463-123-4447",
+  website: "ramiro.info",
+ company: {
+ name: "Romaguera-Jacobson",
+ catchPhrase: "Face to face bifurcated interface",
+  bs: "e-enable strategic applications",
+ },
+ };
+ document.getElementById("tableC").innerHTML= users3.id + "<br>"+"Name:" + users3.name + "<br>" +"Username:" + users3.username + "<br>" + "Email:" + users3.email + "<br>"+ "Street:" + users3.address.street + "<br>" + "Suite:" + users3.address.suite + "<br>"+ "City:"+ users3.address.city + "<br>"+ "Zipcode:" + users3.address.zipcode + "<br>" + "Lat:" + users3.address.geo.lat + "<br>" + "lng:" + users3.address.geo.lng + "<br>" + "phone:" + users3.phone + "<br>" + "Company Name::" + users3.company.name + "<br>" + "Catchphrase:" + users3.company.catchPhrase + "<br>" +"bs:" + users3.company.bs
+
+  const users4= {id: 4,
+ name: "Patricia Lebsack",
+ username: "Karianne",
+  email: "Julianne.OConner@kory.org",
+ address: {
+ street: "Hoeger Mall",
+ suite: "Apt. 692",
+ city: "South Elvis",
+ zipcode: "53919-4257",
+ geo: {
+ lat: "29.4572",
+  lng: "-164.2990",
+ },
+ },
+ phone: "493-170-9623 x156",
+  website: "kale.biz",
+ company: {
+  name: "Robel-Corkery",
+ catchPhrase: "Multi-tiered zero tolerance productivity",
+ bs: "transition cutting-edge web services",
+   },
+  };
+  
+  document.getElementById("tableD").innerHTML= users4.id + "<br>"+"Name:" + users4.name + "<br>" +"Username:" + users4.username + "<br>" + "Email:" + users4.email + "<br>"+ "Street:" + users4.address.street + "<br>" + "Suite:" + users4.address.suite + "<br>"+ "City:"+ users4.address.city + "<br>"+ "Zipcode:" + users4.address.zipcode + "<br>" + "Lat:" + users4.address.geo.lat + "<br>" + "lng:" + users4.address.geo.lng + "<br>" + "phone:" + users4.phone + "<br>" + "Company Name::" + users4.company.name + "<br>" + "Catchphrase:" + users4.company.catchPhrase + "<br>" +"bs:" + users4.company.bs
+
+ const users5= {id: 5,
+       name: "Chelsey Dietrich",
+       username: "Kamren",
+       email: "Lucio_Hettinger@annie.ca",
+       address: {
+         street: "Skiles Walks",
+         suite: "Suite 351",
+         city: "Roscoeview",
+         zipcode: "33263",
+         geo: {
+           lat: "-31.8129",
+           lng: "62.5342",
+         },
+       },
+       phone: "(254)954-1289",
+       website: "demarco.info",
+       company: {
+         name: "Keebler LLC",
+         catchPhrase: "User-centric fault-tolerant solution",
+         bs: "revolutionize end-to-end systems",
+       },
+     };
+     document.getElementById("tableE").innerHTML= users5.id + "<br>"+"Name:" + users5.name + "<br>" +"Username:" + users5.username + "<br>" + "Email:" + users5.email + "<br>"+ "Street:" + users5.address.street + "<br>" + "Suite:" + users5.address.suite + "<br>"+ "City:"+ users5.address.city + "<br>"+ "Zipcode:" + users5.address.zipcode + "<br>" + "Lat:" + users5.address.geo.lat + "<br>" + "lng:" + users5.address.geo.lng + "<br>" + "phone:" + users5.phone + "<br>" + "Company Name::" + users5.company.name + "<br>" + "Catchphrase:" + users5.company.catchPhrase + "<br>" +"bs:" + users5.company.bs   
+
+
+
+
+       const users6= {id: 6,
+       name: "Mrs. Dennis Schulist",
+       username: "Leopoldo_Corkery",
+       email: "Karley_Dach@jasper.info",
+       address: {
+         street: "Norberto Crossing",
+         suite: "Apt. 950",
+         city: "South Christy",
+         zipcode: "23505-1337",
+         geo: {
+           lat: "-71.4197",
+           lng: "71.7478",
+         },
+       },
+       phone: "1-477-935-8478 x6430",
+       website: "ola.org",
+       company: {
+         name: "Considine-Lockman",
+         catchPhrase: "Synchronised bottom-line interface",
+         bs: "e-enable innovative applications",
+       },
+     };
+
+     document.getElementById("tableF").innerHTML= users6.id + "<br>"+"Name:" + users6.name + "<br>" +"Username:" + users6.username + "<br>" + "Email:" + users6.email + "<br>"+ "Street:" + users6.address.street + "<br>" + "Suite:" + users6.address.suite + "<br>"+ "City:"+ users6.address.city + "<br>"+ "Zipcode:" + users6.address.zipcode + "<br>" + "Lat:" + users6.address.geo.lat + "<br>" + "lng:" + users6.address.geo.lng + "<br>" + "phone:" + users6.phone + "<br>" + "Company Name::" + users6.company.name + "<br>" + "Catchphrase:" + users6.company.catchPhrase + "<br>" +"bs:" + users6.company.bs
+     
+      const users7= {id: 7,
+       name: "Kurtis Weissnat",
+       username: "Elwyn.Skiles",
+       email: "Telly.Hoeger@billy.biz",
+       address: {
+         street: "Rex Trail",
+         suite: "Suite 280",
+         city: "Howemouth",
+         zipcode: "58804-1099",
+         geo: {
+           lat: "24.8918",
+           lng: "21.8984",
+         },
+       },
+       phone: "210.067.6132",
+       website: "elvis.io",
+       company: {
+         name: "Johns Group",
+         catchPhrase: "Configurable multimedia task-force",
+         bs: "generate enterprise e-tailers",
+       },
+     };
+
+     document.getElementById("tableG").innerHTML= users7.id + "<br>"+"Name:" + users7.name + "<br>" +"Username:" + users7.username + "<br>" + "Email:" + users7.email + "<br>"+ "Street:" + users7.address.street + "<br>" + "Suite:" + users7.address.suite + "<br>"+ "City:"+ users7.address.city + "<br>"+ "Zipcode:" + users7.address.zipcode + "<br>" + "Lat:" + users7.address.geo.lat + "<br>" + "lng:" + users7.address.geo.lng + "<br>" + "phone:" + users7.phone + "<br>" + "Company Name::" + users7.company.name + "<br>" + "Catchphrase:" + users7.company.catchPhrase + "<br>" +"bs:" + users7.company.bs
+     
+      const users8= {id: 8,
+       name: "Nicholas Runolfsdottir V",
+       username: "Maxime_Nienow",
+       email: "Sherwood@rosamond.me",
+       address: {
+         street: "Ellsworth Summit",
+         suite: "Suite 729",
+         city: "Aliyaview",
+         zipcode: "45169",
+         geo: {
+           lat: "-14.3990",
+           lng: "-120.7677",
+         },
+       },
+       phone: "586.493.6943 x140",
+       website: "jacynthe.com",
+       company: {
+         name: "Abernathy Group",
+         catchPhrase: "Implemented secondary concept",
+         bs: "e-enable extensible e-tailers",
+       },
+     };
+
+     document.getElementById("tableH").innerHTML= users8.id + "<br>"+"Name:" + users8.name + "<br>" +"Username:" + users8.username + "<br>" + "Email:" + users8.email + "<br>"+ "Street:" + users8.address.street + "<br>" + "Suite:" + users8.address.suite + "<br>"+ "City:"+ users8.address.city + "<br>"+ "Zipcode:" + users8.address.zipcode + "<br>" + "Lat:" + users8.address.geo.lat + "<br>" + "lng:" + users8.address.geo.lng + "<br>" + "phone:" + users8.phone + "<br>" + "Company Name::" + users8.company.name + "<br>" + "Catchphrase:" + users8.company.catchPhrase + "<br>" +"bs:" + users8.company.bs
+     
+      const users9= {id: 9,
+       name: "Glenna Reichert",
+       username: "Delphine",
+       email: "Chaim_McDermott@dana.io",
+       address: {
+         street: "Dayna Park",
+         suite: "Suite 449",
+         city: "Bartholomebury",
+         zipcode: "76495-3109",
+         geo: {
+           lat: "24.6463",
+           lng: "-168.8889",
+         },
+       },
+       phone: "(775)976-6794 x41206",
+       website: "conrad.com",
+       company: {
+         name: "Yost and Sons",
+         catchPhrase: "Switchable contextually-based project",
+         bs: "aggregate real-time technologies",
+       },
+     };
+
+     document.getElementById("tableI").innerHTML= users9.id + "<br>"+"Name:" + users9.name + "<br>" +"Username:" + users9.username + "<br>" + "Email:" + users9.email + "<br>"+ "Street:" + users9.address.street + "<br>" + "Suite:" + users9.address.suite + "<br>"+ "City:"+ users9.address.city + "<br>"+ "Zipcode:" + users9.address.zipcode + "<br>" + "Lat:" + users9.address.geo.lat + "<br>" + "lng:" + users9.address.geo.lng + "<br>" + "phone:" + users9.phone + "<br>" + "Company Name::" + users9.company.name + "<br>" + "Catchphrase:" + users9.company.catchPhrase + "<br>" +"bs:" + users9.company.bs
+     
+      const users10 = {id: 10,
+       name: "Clementina DuBuque",
+       username: "Moriah.Stanton",
+       email: "Rey.Padberg@karina.biz",
+       address: {
+         street: "Kattie Turnpike",
+         suite: "Suite 198",
+         city: "Lebsackbury",
+         zipcode: "31428-2261",
+         geo: {
+           lat: "-38.2386",
+           lng: "57.2232",
+         },
+       },
+       phone: "024-648-3804",
+       website: "ambrose.net",
+       company: {
+         name: "Hoeger LLC",
+         catchPhrase: "Centralized empowering task-force",
+         bs: "target end-to-end models",
+       },
+     };
+
+     document.getElementById("tableJ").innerHTML= users10.id + "<br>"+"Name:" + users10.name + "<br>" +"Username:" + users10.username + "<br>" + "Email:" + users10.email + "<br>"+ "Street:" + users10.address.street + "<br>" + "Suite:" + users10.address.suite + "<br>"+ "City:"+ users10.address.city + "<br>"+ "Zipcode:" + users10.address.zipcode + "<br>" + "Lat:" + users10.address.geo.lat + "<br>" + "lng:" + users10.address.geo.lng + "<br>" + "phone:" + users10.phone + "<br>" + "Company Name::" + users10.company.name + "<br>" + "Catchphrase:" + users10.company.catchPhrase + "<br>" +"bs:" + users10.company.bs 
